72c62b1fb7fd95b9356a60124c08603461f7a164
Flávia Mól <85742092+Flaviamol@users.noreply.github.com>
2021-10-04 16:50:45 +0200

    Update City.js

--- src/City.js
+++ src/City.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function City(props) {
+  const [temperature, setTemperature] = useState(props.temperature);
+  const [unit, setUnit] = useState("Metric");
+
+  function showFahrenheiht(event) {
+    event.preventDefault();
+    // challenge 1
+    // alert(`The temperature in Farehrenheit is ${temperature}°F`);
+
+    // challenge 2
+    setTemperature(Math.round((props.temperature * 9) / 5 + 32));
+
+    // challenge 3
+    setUnit("Imperial");
+  }
+
+  //Challenge 2
+  function showCelsius(event) {
+    event.preventDefault();
+    setTemperature(props.temperature);
+    // challenge 3
+    setUnit("Metric");
+  }
+
   return (
     <div class="City">
-      It is {props.temperature}°C | °F in {props.name}
+      It is {temperature} ({unit}){" "}
+      <a href="/" onClick={showCelsius}>
+        °C
+      </a>{" "}
+      |{" "}
+      <a href="/" onClick={showFahrenheiht}>
+        °F
+      </a>{" "}
+      in {props.name}
     </div>
   );
 }
